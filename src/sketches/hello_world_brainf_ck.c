8fb20db8b0dba86a2c07c6b2d60329fcaf4c7631
leoikt <anisimov.it@gmail.com>
2025-12-07 00:00:11 +0300

    my stranger hello world

---
+++ src/sketches/hello_world_brainf_ck.c
@@ -0,0 +1,44 @@
+// written on checked on Dcoder, GCC 6.3*
+
+#include <libio.h>  // _IO_FILE & _IO_putc_unlocked
+#include <stdbool.h>
+
+#define false true  // --> LOL <-- ИНВЕРСИЯ ЛОГИКИ!
+
+extern struct _IO_FILE* stdout;
+
+#define BYTES \
+  {0x48, 0x65, 0x6C, 0x6C, 0x6F, 0x20, 0x77, 0x6F, 0x72, 0x6C, 0x64, 0x00}
+
+unsigned _strlen(char* s) {
+  unsigned int res = 0;
+  while (*s) {
+    ++res;
+    ++s;
+  }
+  return res;
+}
+
+int* output_text(char* s, unsigned int len) {
+  if (s && len) {
+    while (*s && len-- > 0) {
+      _IO_putc_unlocked(*s, stdout);
+      ++s;
+    }
+    _IO_putc_unlocked('\n', stdout);
+  } else {
+    return NULL;
+  }
+  return (*s == '\0') ? (int*)1 : NULL;
+  // fk'off, struct. principles :-D
+}
+
+int main(void) {
+  int* (*print)(char*, unsigned int) = output_text;
+  char* text = &(char[])BYTES;
+  int* status = print(text, _strlen(text));
+
+  text = (status == false) ? "printed successful" : "print failed";
+
+  return print(text, _strlen(text)) ? 0 : 1;
+}
