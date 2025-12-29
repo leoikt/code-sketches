72961a68b3a1e5b7235904dc027344cd50a669f9
leoikt <anisimov.it@gmail.com>
2025-12-29 12:19:16 +0300

    list_destroy foo in list.c

--- src/LLP2024/list.c
+++ src/LLP2024/list.c
@@ -34,3 +34,12 @@ size_t list_length(const struct list* list) {
   }
   return len;
 }
+
+// src: https://stepik.org/lesson/1443630/step/6?unit=1462431
+void list_destroy(struct list* list) {
+  while (list) {
+    struct list* next = list->next;
+    free(list);
+    list = next;
+  }
+}
