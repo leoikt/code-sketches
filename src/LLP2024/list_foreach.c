205975ae06eaf531e519696b216ba6abb9119e40
leoikt <anisimov.it@gmail.com>
2026-01-12 20:30:32 +0300

    add map_mut in List_foreach.c

--- src/LLP2024/list_foreach.c
+++ src/LLP2024/list_foreach.c
@@ -24,3 +24,18 @@ void list_foreach(const struct list* l, void(f)(int64_t)) {
 void list_print(const struct list* l) {
   list_foreach(l, print_int64_with_space);
 }
+
+// src: https://stepik.org/lesson/1443633/step/3?unit=1462434
+
+/* Change each element of the list with the f function */
+void list_map_mut(struct list* l, int64_t(f)(int64_t)) {
+  while (l) {
+    l->value = f(l->value);
+    l = l->next;
+  }
+}
+
+static int64_t triple(int64_t x) { return x * 3; }
+
+/* Using list_map_mut multiply all the elements of the list by 3 */
+void list_triple(struct list* l) { list_map_mut(l, triple); }
